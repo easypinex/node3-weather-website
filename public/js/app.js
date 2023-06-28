9e6d5a7b7566c7bc709127751b1686ffe47c42bf
Andy <easypinex@gmail.com>
2023-06-28 15:04:15 +0800

    Use self-API

--- public/js/app.js
+++ public/js/app.js
@@ -14,39 +14,22 @@ const message2 = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     message1.textContent = 'Loading ...'
+    message2.textContent = ''
     const address = search.value
     if (!address)  {
         message1.textContent = 'You must give search value';
         return
     }
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZWFzeXBpbmV4IiwiYSI6ImNsaWNzN2lpdjBnNnYzaHF2bGNrdGhseTIifQ.ldFtXeV-Qm2vEq6kCG3kAw`).then(
+    fetch(`/weather?address=${encodeURIComponent(address)}`).then(
     (response) => {
         response.json().then(data => {
-            console.log('mapbox', data)
+            console.log('data', data)
             if (data.error) {
                 message1.textContent = data.error;
                 return console.log(data.error)
             }
-            if (data.features.length == 0) {
-                message1.textContent = "Unable to find location. Try another seatch.";
-                return
-            }
-            const location = data.features[0].place_name
-            const latitude = data.features[0].center[1];
-            const longtitude = data.features[0].center[0];
-            const weather_url = `http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=${latitude},${longtitude}`
-            fetch(weather_url).then((response) => {
-                response.json().then(data => {
-                    console.log('weatherstack', data)
-                    if (data.error) {
-                        message1.textContent = data.error.info;
-                        return console.log(data.error.info)
-                    }
-                    const forecast = data.current.weather_descriptions[0]
-                    message1.textContent = location
-                    message2.textContent = forecast
-                })
-            })
+            message1.textContent = data.location
+            message2.textContent = data.forecast
         })
     }
 )
