3b63f343d11f40ad5f160ca85e4e1dde0eeea138
Andy <easypinex@gmail.com>
2023-06-29 14:07:23 +0800

    support port

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 
 
 const app = express()
+const port = process.env.PORT || 3000
+
 // Define path for Express config
 const staticDir = path.join(__dirname, '..', 'public')
 const viewsPath = path.join(__dirname, '..', 'templates', 'views')
@@ -92,6 +94,6 @@ app.get('*', (req, res) => {
         name: 'Perry'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
